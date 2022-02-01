f87a9d9319d232c491bb11ecfc8121b9c110f2cb
takometonidze01 <takometonidze2012@gmail.com>
2022-02-01 23:48:43 +0400

    create some file and structure it

---
+++ TikTok/Managers/APICaller.swift
@@ -0,0 +1,8 @@
+//
+//  APICaller.swift
+//  TikTok
+//
+//  Created by CodergirlTM on 01.02.22.
+//
+
+import Foundation
---
+++ TikTok/Managers/DatabaseManager.swift
@@ -0,0 +1,8 @@
+//
+//  DatabaseManager.swift
+//  TikTok
+//
+//  Created by CodergirlTM on 01.02.22.
+//
+
+import Foundation
