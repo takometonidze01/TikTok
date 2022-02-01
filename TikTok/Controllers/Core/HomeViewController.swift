05438ee5d097c38280014d4d15070afc90dca5bd
takometonidze01 <takometonidze2012@gmail.com>
2022-02-02 00:55:58 +0400

    tab bar set up

--- TikTok/Controllers/Core/HomeViewController.swift
+++ TikTok/Controllers/Core/HomeViewController.swift
@@ -11,6 +11,8 @@ class HomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+
         // Do any additional setup after loading the view.
     }
 
